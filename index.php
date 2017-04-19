380b264d40951732f9c3a05f521606fed63c28ce
Nick Stielau <nick.stielau@gmail.com>
2017-04-19 10:04:39 -0700

    Fixing PHP varnamne.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $db_name = $_ENV["DB_NAME"];
 $db_host = $_ENV["DB_HOST"];
 $db_user = $_ENV["DB_USER"];
 $db_port = $_ENV["DB_PORT"];
-$db_passworld = $_ENV["DB_PASSWORD"];
+$db_password = $_ENV["DB_PASSWORD"];
 
 if (!mysql_connect($db_host, $db_user, $db_password)) {
     echo 'Could not connect to mysql';
